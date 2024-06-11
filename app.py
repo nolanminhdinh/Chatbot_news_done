8481e42506575a1d9a6971a5e6fa0b0e01637514
Phạm Cường Quốc <167053034+pcq3014@users.noreply.github.com>
2024-06-11 11:51:35 +0700

    app.py

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ lemmatizer = WordNetLemmatizer()
 with open('news_scrap_data.json', encoding='utf-8') as file:
     news_scrap_data = json.load(file)
 
+# Load dữ liệu intents từ file data.json
+with open('data.json', encoding='utf-8') as file:
+    intents = json.load(file)
+
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
@@ -29,38 +33,47 @@ def bow(sentence, words, show_details=True):
                     print("found in bag: %s" % w)
     return np.array(bag)
 
-import random
+def classify_intent(msg):
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            if pattern.lower() in msg.lower():
+                return intent
+    return None
 
 def chatbot_response(msg):
     categories = ["Thời sự", "Góc nhìn", "Thế giới", "Podcasts", "Kinh doanh", "Bất động sản", "Khoa học", 
                   "Giải trí", "Thể thao", "Pháp luật", "Giáo dục", "Sức khỏe", "Đời sống", "Du lịch", 
                   "Số hóa", "Xe", "Ý kiến", "Tâm sự", "Thư giãn"]
-    
+
+    intent = classify_intent(msg)
+    if intent:
+        return random.choice(intent['responses'])
+
     selected_category = None
     for category in categories:
         if category.lower() in msg.lower():
             selected_category = category
             break
-    
+
     news_responses = []
-    
+
     if selected_category:
         # Lọc ra tất cả tin tức trong danh mục được chọn
         selected_news = [news for news in news_scrap_data['intents'] if news['category'] == selected_category]
-        
+
         # Kiểm tra xem có tin tức nào trong danh mục không
         if selected_news:
             # Chọn một tin tức ngẫu nhiên từ danh sách tin tức đã lọc
             random_news = random.choice(selected_news)
-            
+
             title = random_news['title']
             summary = random_news['summary']
             link = random_news['news_link']
-            
+
             news_responses.append(f"<strong>Tiêu đề:</strong> {title}<br> \n <strong>Nội dung:</strong> {summary}<br> \n <strong>Chủ đề:</strong> {random_news['category']}<br> \n <a href='{link}' target='_blank'>Xem thêm</a><br><br>")
         else:
             news_responses.append("Không có tin tức nào trong chủ đề này.")
-            
+
         return "".join(news_responses)
 
     elif 'tin tức' in msg.lower():
@@ -70,7 +83,6 @@ def chatbot_response(msg):
         summary = news['summary']
         link = news['news_link']
         return f"<strong>Tiêu đề:</strong> {title}<br> \n <strong>Nội dung:</strong> {summary}<br> \n <strong>Chủ đề:</strong> {news['category']}<br> \n <a href='{link}' target='_blank'>Xem thêm</a><br><br>"
-
 
 app = Flask(__name__)
 app.static_folder = 'static'
